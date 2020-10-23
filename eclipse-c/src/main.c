804a6061a2b348121e0dd742d11d7344f924bfd0
rohanbari <rohanbari4@gmail.com>
2020-10-23 23:57:22 +0530

    Initial commit

---
+++ eclipse-c/src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : eclipse-c.c
+ Author      : Rohan Bari
+ Version     : v0.0
+ Copyright   : MIT License by Rohan Bari
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    puts("Welcome to C-Eclipse!");
+
+    return EXIT_SUCCESS;
+}
